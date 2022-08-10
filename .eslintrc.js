3519063ab646bdf975038b2f2084ca504a64cf57
lqs <957698457@qq.com>
2022-08-10 17:23:54 +0800

    init

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: [require.resolve('@umijs/fabric/dist/eslint')],
-  rules: {
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    'eslintobject-shorthand': ['always'],
-  },
-  globals: {},
-};
--- src/services/user.ts
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request';
-
-export async function queryCurrent(): Promise<any> {
-  return request('/user/info');
-}
-
-export async function queryMessage(): Promise<any> {
-  return request('/user/message');
-}
